6c1fbf4222858fd1c212c0c65be1c7050584e77c
r3workz <122603849+r3workz@users.noreply.github.com>
2023-09-12 22:35:00 +0000

    attempt1

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 function calculateMinCost() {
   //your code here
+  const ropes = document.getElementById("rope-lengths").value.split(",")
+
+  let cost=0
   
+  while(ropes.length>1){
+    ropes.sort((a,b)=>a-b)
+
+    let temp = Number.parseInt(ropes[0])+Number.parseInt(ropes[1])
+    ropes.shift()
+    ropes.shift()
+    ropes.push(temp)
+
+    cost +=temp
+
+    // console.log(ropes);
+  }
+  // console.log(cost);
   
-  
+  document.getElementById("result").innerText = cost
 }  
